de0766d8400eec307f91b4b28fc643a2cfec542a
asd45618 <asd45618@naver.com>
2024-08-17 17:26:01 +0900

    초기 세팅

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
 import { Header } from './components/layout/Header';
 import { Footer } from './components/layout/Footer';
 import { Outlet } from 'react-router';
+import styled from 'styled-components';
+
+const LayoutWrapper = styled.div`
+  background: #e4e4e4;
+  main {
+    height: 100vh;
+  }
+`;
 
 export const Layout = () => {
   return (
-    <div>
+    <LayoutWrapper>
       <Header />
       <main>
         <Outlet />
       </main>
       <Footer />
-    </div>
+    </LayoutWrapper>
   );
 };
